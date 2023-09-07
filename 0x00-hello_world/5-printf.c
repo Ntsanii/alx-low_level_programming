05803f9f7e9e4bdec81c11b011222e41325eff0b
Ntsane Monaheng <ntsane01m@gmail.com>
2023-09-07 16:12:01 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main part of the program
+ *
+ * Return: for an integer
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
