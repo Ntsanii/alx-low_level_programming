0f0d4faa354ca870ff76b057fdbe86e894aaae23
Ntsane Monaheng <ntsane01m@gmail.com>
2023-09-10 13:26:23 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - body of the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
