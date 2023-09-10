4dc164b05947cbe118cd7b65ba0440ee5cd5af7e
Ntsane Monaheng <ntsane01m@gmail.com>
2023-09-10 13:06:03 -0700

    When i was having soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - body of the program
+ *
+ * Return: is always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	return (0);
+}
