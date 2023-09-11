b9072ca0188b0a3074af0cf83cf0c86424e62ab2
Ntsane Monaheng <ntsane01m@gmail.com>
2023-09-11 11:20:07 -0700

    patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,21 @@
 /**
  * main - program body
  *
- * Return: always zero
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
-	char x;
-	
+
 	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i + '0');
-	for (x = 'a' ; x <= 'f' ; x++)
-		putchar(x);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
-	
