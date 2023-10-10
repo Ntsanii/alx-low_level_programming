3629b7d3cc5c022211a2331102ad69bee35d3bce
Ntsane Monaheng <ntsane01m@gmail.com>
2023-10-10 14:45:54 -0700

    new naem for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,18 @@
  * Description: first struct with alx
  */
 
-struct dog
+/**
+ * dog_t - new name for structdog
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
