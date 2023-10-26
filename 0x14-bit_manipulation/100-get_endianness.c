c0cb3f5e00b66f53caa557b280038e59b4969d50
Ntsanii <ntsane01m@gmail.com>
2023-10-26 15:20:18 -0700

    crackme

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - program checks endianness
+ * Return: 0 big endian, 1 little endian
+ */
+int get_endianness(void)
+{
+	int a;
+	char *b;
+
+	a = 1;
+	b = (char *)&a;
+	return (*b);
+}
